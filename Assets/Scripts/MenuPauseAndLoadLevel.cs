98b830de7a9d392e31769e759a5a932a6f273235
YOUSSOUF ISMAIL CHERIFI <youcef.ismail.cherifi@gmail.com>
2019-05-09 18:55:48 +0200

    Cloning project from laptop

---
+++ Assets/Scripts/MenuPauseAndLoadLevel.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuPauseAndLoadLevel : MonoBehaviour {
+
+	public string levelToLoad;
+	public float delay = 2f;
+
+    void Awake()
+    {
+#if UNITY_STANDALONE || UNITY_WEBGL
+        Camera.main.orthographicSize = 5f * 1.777251f / Camera.main.aspect;
+#else
+        Camera.main.aspect= 16f/9f;
+#endif
+    }
+
+    // use invoke to wait for a delay then call LoadLevel
+    void Update () {
+		Invoke("LoadLevel",delay);
+	}
+
+	// load the specified level
+	void LoadLevel() {
+		Application.LoadLevel(levelToLoad);
+	}
+}
